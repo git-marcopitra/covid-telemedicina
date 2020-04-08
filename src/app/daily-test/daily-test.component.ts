42a2b5e8de6876c47b3fe3cad7efeebb49bfbc29
git-marcopitra <git-marcopitra>
2020-04-08 20:27:26 +0200

    UPDATE: Test implementation

--- src/app/daily-test/daily-test.component.ts
+++ src/app/daily-test/daily-test.component.ts
@@ -15,8 +15,19 @@ export class DailyTestComponent implements OnInit {
     gender: ['', Validators.required],
     age: [null, Validators.required],
     docId: ['', Validators.required],
-    travel: [0],
-    people: [0]
+    travel: [false],
+    people: [false],
+    covid: [false],
+    febre: [false],
+    tosse: [false],
+    fadiga: [false],
+    respiracao: [false],
+    garganta: [false],
+    calafrios: [false],
+    corpo: [false],
+    cabeca: [false],
+    coriza: [false],
+    espirros: [false]
   })
 
   geoLocation: {
@@ -68,6 +79,35 @@ export class DailyTestComponent implements OnInit {
     }
   }
   onSubmit() {
+    let travel = this.fc.travel.value ? 5 : 0
+    let people = this.fc.people.value ? 5 : 0
+    let covid = this.fc.covid.value ? 10 : 0
+    let febre = this.fc.febre.value ? 13.12 : 0
+    let tosse = this.fc.tosse.value ? 13.12 : 0 
+    let fadiga = this.fc.fadiga.value ? 13.12 : 0
+    let respiracao = this.fc.respiracao.value ? 6.56 : 0
+    let garganta = this.fc.garganta.value ? 6.56 : 0
+    let calafrios = this.fc.calafrios.value ? 6.56 : 0
+    let corpo = this.fc.corpo.value ? 6.56 : 0
+    let cabeca = this.fc.cabeca.value ? 6.56 : 0
+    let coriza = this.fc.coriza.value ? 3.93 : 0
+    let espirros = this.fc.espirros.value ? 3.91 : 0
+    
+    let level = travel + 
+                people + 
+                covid + 
+                febre + 
+                tosse + 
+                fadiga + 
+                respiracao + 
+                garganta + 
+                calafrios + 
+                corpo + 
+                cabeca + 
+                coriza + 
+                espirros
+
+
 
   }
 }
