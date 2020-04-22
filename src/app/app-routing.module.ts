76355c174d68f44bb958ae9db356fb79b4fdf3a0
git-marcopitra <git-marcopitra>
2020-04-23 00:06:57 +0100

    Semi-Final Version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   { path: 'aboutus', component: AboutusComponent },
   { path: 'statistic', component: StatisticComponent },
   { path: 'dailytest', canActivate: [AuthGuard],component: DailyTestComponent },
-  { path: 'telemedicina',/* canActivate: [AuthGuard],*/ loadChildren: () => import('./telemedicina/telemedicina.module').then(mod => mod.TelemedicinaModule)}
+  { path: 'telemedicina', canActivate: [AuthGuard], loadChildren: () => import('./telemedicina/telemedicina.module').then(mod => mod.TelemedicinaModule)}
 ];
 
 @NgModule({
